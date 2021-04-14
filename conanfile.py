318464f70d7654009483a699236a4dedf0a5307e
bincrafters-user <bincrafters@gmail.com>
2021-04-14 23:40:42 +0000

    Update Conan conventions
    Automatically created by bincrafters-conventions 0.30.4

--- conanfile.py
+++ conanfile.py
@@ -82,7 +82,7 @@ class FluidSynthConan(ConanFile):
 
     _source_subfolder = "source_subfolder"
     _build_subfolder = "build_subfolder"
-    requires = "glib/2.67.6"
+    requires = "glib/2.68.0"
 
     def configure(self):
         del self.settings.compiler.libcxx
